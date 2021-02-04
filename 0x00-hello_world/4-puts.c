39edfb802722aa5bf55f523e38d7dc27885f6f23
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-04 04:28:03 +0000

    new changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char string[60];
+  strcpy(string, "Programming is like building a multilingual puzzle");
+  puts(string);
+  return 0;
+}
