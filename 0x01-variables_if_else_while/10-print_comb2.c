4a44f3cf9d7a18e43364eed97007df9af78fe100
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-05 04:53:32 +0000

    new changes

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,5 +6,27 @@
  */
 int main(void)
 {
-
+int x = '0';
+int y = '0';
+int space = ' ';
+int comma = ',';
+while (x <= '9')
+{
+while (y <= '9')
+{
+putchar(x);
+putchar(y);
+if (x == '9' && y == '9')
+{
+break;
+}
+putchar(comma);
+putchar(space);
+y++;
+}
+y = '0';
+x++;
+}
+putchar('\n');
+return (0);
 }
