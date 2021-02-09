308aaebad2f2ab0ca93543d3ad1a8d63a1c047b3
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-09 21:07:09 +0000

    new changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- *
- *
+ * jack_bauer - prints 24 clock
+ * Return: 0
  *
  */
 void jack_bauer(void)
 {
-  int a;
-  int b;
-  int c;
-  int d = '0';
+int a;
+int b;
+int c;
+int d = '0';
   while (d <= '2')
     {
   for (c = '0'; c <= '9'; c++)
