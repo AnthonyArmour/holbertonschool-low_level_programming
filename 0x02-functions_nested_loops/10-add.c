c1dcbc1a048a2baf7a6be209dfa3f68750c6a59e
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-10 19:42:18 +0000

    new changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * add - adds nums
+ * @x: num
+ * @y: num
+ * Return: 0
+ */
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
+int add(int x, int y);
