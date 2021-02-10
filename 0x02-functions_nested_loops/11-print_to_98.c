c6153eb1d9d0f0cdfab759ccafd24550a536ea53
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-10 06:03:37 +0000

    new changes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * 
+ *
+ *
+ */
+void print_to_98(int n);
+{
+	while (n< 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}
