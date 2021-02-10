ef04f62dfac1f340bdffbc4a8ffc77763c7f9cbe
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-10 19:04:32 +0000

    new changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * 
- *
- *
+ * print_to_98 - prints to 98
+ * @n: input
+ * Return: 0
  */
-void print_to_98(int n);
+void print_to_98(int n)
 {
-	while (n< 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
