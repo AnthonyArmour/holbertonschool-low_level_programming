4b39504caaba1f58fe1e862c1e9d2d6bb46b196d
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-11 23:10:25 +0000

    new changes

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,29 @@
 #include "holberton.h"
+/**
+ * print_triangle - prints triangle
+ * @size: input
+ * Return: none
+ */
+void print_triangle(int size)
+{
+	int y;
+	int x;
+	int z;
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < (size - x); y++)
+			{
+				_putchar(' ');
+			}
+			for (z = 0; z < (x + 1); z++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
