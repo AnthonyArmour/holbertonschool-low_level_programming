3cf0aabc1198bea8359c38a3219798e1328379ea
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-11 22:14:06 +0000

    new changes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,25 @@
-#include "holberton.c"
+#include "holberton.h"
+/**
+ * print_square - prints square
+ * @size: input
+ * Return: none
+ */
+void print_square(int size)
+{
+	int x = 0;
+	int y = 0;
+
+	if (size > 0)
+	{
+		for (; x < size; x++)
+		{
+			for (; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,30 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+ * main - replace multiples of 3
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 1;
+
+	for (; x <= 99; x++)
+	{
+		if (x % 3 == 0 && x % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		if (x % 5 == 0 && x % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		if (x % 5 == 0 && x % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (x % 5 != 0 && x % 3 != 0)
+			printf("%i ", x);
+	}
+	printf("Buzz");
+	return (0);
+}
