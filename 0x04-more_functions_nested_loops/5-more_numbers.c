532906845809ef37f75e66dbeb244c6f3e116fc4
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-11 20:46:07 +0000

    new changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,22 @@ void more_numbers(void)
 	int x = '0';
 	int y = '0';
 	int b = '0';
+
 	for (; x <= '9'; x++)
 	{
 		for (; y <= '1'; y++)
 		{
-		        for (; b <= '9'; b++)
+			for (; b <= '9'; b++)
 			{
 				if (y == '1' && b == '5')
 				{
 					break;
-			        }
+				}
 				if (y == '1')
 				{
 					_putchar(y);
 }
 				_putchar(b);
-				
 			}
 			b = '0';
 		}
