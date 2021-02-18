e5ecf0c4d4eb03e198da7ded00998eef9a56e314
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-18 18:49:22 +0000

    new changes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	int x = 0;
+	for (; s1[x] != '\0'; x++)
 	{
-		return (15);
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
 	}
-	if (s1 < s2)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (s1[x] - s2[x]);
 }
