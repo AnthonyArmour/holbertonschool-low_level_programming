103966e7d3861ba97d91a79d2d1a0daa9ea88321
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-19 02:03:16 +0000

    new changes to rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -12,9 +12,6 @@ char *rot13(char *x)
 	char num[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int yy;
 
-	_putchar(letter[26]);
-	_putchar(num[26]);
-	_putchar('\n');
 	for (; x[y] != '\0'; y++)
 	{
 		for (yy = 0; letter[yy] != '\0'; yy++)
