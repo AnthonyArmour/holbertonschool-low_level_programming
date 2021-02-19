494e53a47400440393fbe90b6ea6f5446624ac8f
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-19 00:57:28 +0000

    new changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,20 +3,21 @@
  * _strncpy - copies a string
  * @dest: str input
  * @src: str input
+ * @n: input
  * Return: str
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int src_len = 0;
 	int x = 0;
 
-	while (src[src_len] != '\0')
-	{
-		src_len++;
-	}
-	for (; x < n && x <= src_len; x++)
+	for (; x < n && src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
+	}
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
 	}
 	return (dest);
 }
