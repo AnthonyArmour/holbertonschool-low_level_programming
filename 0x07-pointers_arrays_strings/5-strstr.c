47a90f733473bbe49bc1a75fd8512a2d583b7a18
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-22 21:42:22 +0000

    new changes to task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string input
+ * @needle: string input
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0;
+	int y = 0;
+	int n = 0;
+	char *p;
+
+	for (; haystack[x] != '\0'; x++)
+	{
+		for (y = x; haystack[y] == needle[n]; y++)
+		{
+			n++;
+			if (needle[n] == '\0')
+			{
+				p = &haystack[x];
+				return (p);
+			}
+			continue;
+
+		}
+		n = 0;
+	}
+	return ('\0');
+}
