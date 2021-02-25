80080851a1ded90931814429ecb34dc4b674495b
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-25 20:43:56 +0000

    new changes to task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,22 @@
 #include "holberton.h"
 /**
  * num - stuff
+ * @n: input
  * @x: input
- * @y: input
  * Return: int
  */
-int num(int x, int y)
+int num(int n, int x)
 {
-	float p = y / x;
-	int pp = p;
 
-	if (x == 1)
-	{
-		return (1);
-	}
-	if ((p - pp) == 0)
+	if (n % x == 0)
 	{
 		return (0);
 	}
-	return (num((x - 1), y));
+	if (x > (n / 2))
+	{
+		return (1);
+	}
+	return (num(n, (x + 1)));
 }
 
 /**
@@ -29,5 +27,9 @@ int num(int x, int y)
  */
 int is_prime_number(int n)
 {
-	return (num((x - 1), y));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (num(n, 2));
 }
