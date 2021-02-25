14031683de319bc33f99f5057edd888b3791a3fd
AnthonyArmour <anthonydarmour@gmail.com>
2021-02-25 00:31:26 +0000

    new changes to task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - returns square root
+ * num - sqrt
+ * @n: int inputt
+ * @x: input
+ * @y: input
+ * Return: int and float
+ */
+float num(float x, float y)
+{
+	if ((x * x) == y)
+	{
+		return (x);
+	}
+	if (x <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (num((x - 1), y));
+	}
+}
+
+
+
+
+
+
+int _sqrt_recursion(int n)
+{
+
+	if (num(n, n) == -1)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (num(n, n));
+	}
+}
