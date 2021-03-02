aa67b44fe7a9ec091f470590dfffc4447ffcad7a
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-02 18:22:51 +0000

    fixed betty for create array task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 #include "holberton.h"
+/**
+ * create_array - creates array in heap
+ * @size: int
+ * @c: char
+ * Return: char
+ *
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *ptr = (char *)malloc(size * sizeof(char));
