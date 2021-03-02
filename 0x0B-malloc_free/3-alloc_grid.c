b9e2436786e1a536559295585720254b6685d1b3
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-02 22:46:02 +0000

    new changes to task 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,17 +11,18 @@ int **alloc_grid(int width, int height)
 {
 	int *r, **g;
 	int i, ii, s;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	s = (sizeof(int *) * height) + (sizeof(int) * width * height);
 	g = (int **)malloc(s);
 	if (g == NULL)
 		return (NULL);
-        r = (int *)(g + height);
-        for (i = 0; i < height; i++)
-                g[i] = (r + width * i);
-        for (i = 0; i < height; i++)
-                for (ii = 0; ii < width; ii++)
-                        g[i][ii] = 0;
-        return (g);
+	r = (int *)(g + height);
+	for (i = 0; i < height; i++)
+		g[i] = (r + width * i);
+	for (i = 0; i < height; i++)
+		for (ii = 0; ii < width; ii++)
+			g[i][ii] = 0;
+	return (g);
 }
