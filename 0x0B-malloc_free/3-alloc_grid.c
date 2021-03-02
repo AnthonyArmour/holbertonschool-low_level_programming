cf826e9bcfde5debb2764cc5db784f1e16d3dae2
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-02 18:51:00 +0000

    new changes to 4th task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int y = 0;
 	int **d2_array = (int **)malloc(width * sizeof(int *));
 
-	if (width < 1 || height < 1)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	for (; x < width; x++)
 	{
