84cde863657eb87524adf4c0bee758bfdb6e8008
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-04 20:28:45 +0000

    added putchar to header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,4 +6,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
+int _putchar(char c);
 #endif
