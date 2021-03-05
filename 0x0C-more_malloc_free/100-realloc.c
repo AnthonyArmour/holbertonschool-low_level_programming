fdea09b48c0e76aadf14c1cf6660f657aa478a65
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-05 00:51:56 +0000

    new changes to task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,8 +13,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
+		nptr = malloc(new_size);
+		if (nptr == NULL)
 			return (NULL);
 	}
 	if (new_size == old_size)
