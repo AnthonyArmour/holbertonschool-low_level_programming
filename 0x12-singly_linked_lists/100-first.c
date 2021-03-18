b85e37ec3e7f3f7547e31d21793fa9677f3ca139
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-18 23:47:46 +0000

    task 6 added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+/**
+ * premain - premain
+ */
+#include <stdio.h>
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my");
+	printf(" back!\n");
+}
