f889729d8af59846edca6936da0c73a60237a85a
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-18 23:23:45 +0000

    task 101 added

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+	SECTION .data
+msg:		 db "Hello, Holberton", 10
+
+	SECTION .text
+	global main
+main:
+	mov edx, 17
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
