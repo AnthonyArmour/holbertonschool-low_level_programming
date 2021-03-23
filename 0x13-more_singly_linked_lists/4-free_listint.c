4c337aa2edba96b11493ca1f627e52c084a36842
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-23 18:19:17 +0000

    changed task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,11 +10,10 @@ void free_listint(listint_t *head)
 	listint_t *temp;
 
 	temp = head;
-	while (temp->next != NULL)
+	while (temp)
 	{
 		temp = temp->next;
 		free(head);
 		head = temp;
 	}
-	free(temp);
 }
