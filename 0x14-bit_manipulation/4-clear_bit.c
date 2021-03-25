3aa425738cb5e697d16ff74ee58efd358c4264d5
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-25 23:29:43 +0000

    fixed task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -16,7 +16,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	num = _pow_recursion(2, index);
 	if (num > (unsigned long)*n)
-		return (-1);
+		return (1);
 	if (!((*n >> index) & 1) || *n == 0)
 		return (1);
 	*n -= (unsigned long)num;
