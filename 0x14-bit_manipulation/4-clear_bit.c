c44699d379fbf9337b31a440a6d9f991363b26b5
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-25 22:15:02 +0000

    adding task 4 and adjusted .h file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * clear_bit - sets bit at index to 0
+ * @n: num
+ * @index: bit index of num
+ * Return: 1 or -1 for error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num;
+
+	if (index > 63)
+		return (-1);
+	if (!n)
+		return (-1);
+	num = _pow_recursion(2, index);
+	printf("this is num: %u\n", num);
+	if (((*n >> index) & 0) || *n == 0)
+		return (1);
+	*n -= (unsigned long)num;
+	return (1);
+}
+/**
+ * _pow_recursion - x to the power y
+ * @x: num
+ * @y: exponent
+ * Return: int
+ */
+unsigned int _pow_recursion(unsigned int x, unsigned int y)
+{
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
