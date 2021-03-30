eb61a5420f9f4a35f7bda7ae4a1d40667182c707
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-30 20:25:13 +0000

    added task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * create_file - name
+ * @filename: name
+ * @text_content: str
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t wcheck;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	wcheck = write(fd, text_content, _strlen(text_content));
+	if (wcheck == -1)
+		return (-1);
+	close(fd);
+	return (1);
+
+}
+/**
+ * _strlen - len
+ * @s: str
+ * Return: len
+ */
+ssize_t _strlen(char *s)
+{
+	ssize_t x = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		continue;
+	}
+	return (x);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,5 +8,6 @@
 #include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
-
+int create_file(const char *filename, char *text_content);
+ssize_t _strlen(char *s);
 #endif
