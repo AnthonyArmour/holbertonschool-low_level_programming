ef666cb426062dad9d371e3bb455b9c24c7063a6
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-30 20:28:17 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
